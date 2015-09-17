86ede91afa6c85416370db669e48c0f70dfeb86e
Denny <Denny@104.39.84.32>
2015-09-16 21:19:35 -0400

    john now

--- TowerDefense/src/towerdefense/MainMenuGUI.java
+++ TowerDefense/src/towerdefense/MainMenuGUI.java
@@ -5,10 +5,36 @@
  */
 package towerdefense;
 
+import javax.swing.*;
 /**
  *
  * @author ala5322
  */
-public class MainMenuGUI {
+public class MainMenuGUI
+{
+    private void initComponents()
+    {
+        JFrame mainFrame = new JFrame();
+        JPanel mainMenuPanel = new JPanel();
+        
+        JLabel nameLabel = new JLabel("Please enter your name, son");
+        JLabel difficultyLabel = new JLabel("Please select difficulty");
+        JTextField nameTextBox = new JTextField("Hugh Jaynus");
+        
+        String [] difficulties = new String[3];
+        difficulties[0]="Easy-Peasy";
+        difficulties[1]="Middle-of-the-Road";
+        difficulties[2]="Hard-as-F***";
+        
+        JComboBox difficultyComboBox = new JComboBox(difficulties);
+        
+        mainMenuPanel.add(nameLabel);
+        mainMenuPanel.add(nameTextBox);
+        mainMenuPanel.add(difficultyLabel);
+        mainMenuPanel.add(difficultyComboBox);
+        
+        mainFrame.add(mainMenuPanel);
+    
+    }
     
 }
