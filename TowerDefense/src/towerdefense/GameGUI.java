0748e84fa675ff2096031719051f62de4cda2e58
ala5322 <ala5322@206-L36.up.ist.local>
2015-09-30 20:07:31 -0400

    Merge origin/master
    Conflicts:
    	TowerDefense/src/towerdefense/Game.java

--- TowerDefense/src/towerdefense/GameGUI.java
+++ TowerDefense/src/towerdefense/GameGUI.java
@@ -6,6 +6,7 @@
 package towerdefense;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -13,6 +14,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  *
@@ -20,34 +22,55 @@ import javax.swing.JPanel;
  */
 public class GameGUI 
 {
-    private void initComponents()
+    public void initComponents()
     {
+        final int WIDTH_ONE = 900;
+        final int WIDTH_TWO = 300;
+        final int HEIGHT = 600;
+        //create the JFrame and set size
         JFrame fullFrame = new JFrame();
-        fullFrame.setSize(900, 600);
+        fullFrame.setSize(WIDTH_ONE, HEIGHT);
         
+        //create the content pane and set the layout and size
         Container contentPane = fullFrame.getContentPane();
-        
         contentPane.setLayout(new BorderLayout());
-        contentPane.setSize(900, 600);
+        contentPane.setSize(WIDTH_ONE, HEIGHT);
         
+        //create the control and game panels. Set size and color as well.
         JPanel gamePanel = new JPanel(new FlowLayout());
+        gamePanel.setPreferredSize(new Dimension(HEIGHT, HEIGHT));
         JPanel controlPanel = new JPanel(new FlowLayout());
-        gamePanel.setSize(600, 600);
-        controlPanel.setSize(300, 600);
+        controlPanel.setPreferredSize(new Dimension(WIDTH_TWO, HEIGHT));
+        controlPanel.setBackground(Color.gray);
+        gamePanel.setBackground(Color.blue);
         
-        JLabel test1 = new JLabel("here is the game board");
-        JLabel test2 = new JLabel("here is the control panel");
+        //temp labels
+        JTextField moneyField = new JTextField(10);
+        JTextField scoreField = new JTextField(10);
+        //add the weapon options to the control panel
+        //add score, money and etc to control panel
+        JLabel moneyLabel = new JLabel("Money: ");
+        JLabel scoreLabel = new JLabel("Score: ");
         
-        gamePanel.add(test1);
-        controlPanel.add(test2);
-               
+        JPanel moneyPanel = new JPanel(new BorderLayout());
+        moneyPanel.setBackground(Color.gray);
+        moneyPanel.add(moneyLabel, BorderLayout.NORTH);
+        moneyPanel.add(moneyField, BorderLayout.SOUTH);
+        
+        JPanel scorePanel = new JPanel(new BorderLayout());
+        scorePanel.setBackground(Color.gray);
+        scorePanel.add(scoreLabel, BorderLayout.NORTH);
+        scorePanel.add(scoreField, BorderLayout.SOUTH);
+        
+        controlPanel.add(moneyPanel);
+        controlPanel.add(scorePanel);
+        
+        //add the game and control panels to the content pane     
         contentPane.add(gamePanel, BorderLayout.WEST);
         contentPane.add(controlPanel,BorderLayout.EAST);
         
+        fullFrame.pack();
         fullFrame.setVisible(true);
         fullFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //not finished yet
-        
     }
 }
